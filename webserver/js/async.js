a0652fc63b30012bd6a39f636e10f282ea557fbd
r-jb <24355753+r-jb@users.noreply.github.com>
2022-05-05 22:47:06 +0100

    webui now interacts with the script asynchronously

--- webserver/js/async.js
+++ webserver/js/async.js
@@ -1,16 +1,14 @@
-// https://www.w3schools.com/js//js_asynchronous.asp
-
 // Change visuals when an element is running
 function set_visuals_running(element) {
-	document.getElementById(element.id).value = 'false';
+	document.getElementById(element.id).value = 'stop';
 	document.getElementById(element.id).innerText = 'Stop';
 	document.getElementById(element.id).parentElement.className = 'element_on';
 }
 
 // Change visuals when an element is stopped
 function set_visuals_stopped(element) {
-	document.getElementById(element.id).value = 'true';
-	document.getElementById(element.id).innerText = 'Run';
+	document.getElementById(element.id).value = 'start';
+	document.getElementById(element.id).innerText = 'Start';
 	document.getElementById(element.id).parentElement.className = 'element_off';
 }
 
@@ -21,14 +19,19 @@ async function toggle(html_element, script_element_type, script_element_value) {
 		const get_request = await fetch('php/toggle.php?type=' + script_element_type + '&value=' + script_element_value + '&state=' + to_state);
 		const get_response = await get_request.text();
 
-		// Log response for debugging
-		console.log(html_element.id + ' called for ' + script_element_value + ' of type ' + script_element_type + ' to be ' + to_state  + ': ' + get_response);
-
 		// Set button state accordingly
-		if (get_response === '1') {
-			set_visuals_running(html_element);
-		} else {
-			set_visuals_stopped(html_element);
+		if (to_state === 'start') {
+			if (get_response === '0') set_visuals_running(html_element);
+			else {
+				alert('Error: module ' + script_element_value + ' did not start !')
+				set_visuals_stopped(html_element);
+			}
+		} else if (to_state === 'stop') {
+			if (get_response === '0') set_visuals_stopped(html_element);
+			else {
+				alert('Error: module ' + script_element_value + ' did not stop !')
+				set_visuals_running(html_element);
+			}
 		}
 
 	} catch (error) {
@@ -36,29 +39,33 @@ async function toggle(html_element, script_element_type, script_element_value) {
 	}
 }
 
-// ! TODO
-// Returns the state of a module or bundle
-async function check_state(html_element, script_element_type, script_element_value) {
+// Check if a module/bundle is running
+async function check(html_element) {
 	try {
-		const get_request = await fetch('php/check.php?type=' + script_element_type + '&value=' + script_element_value);
+		const get_request = await fetch('php/toggle.php?value=' + html_element.id);
 		const get_response = await get_request.text();
 
 		// Set button state accordingly
-		if (get_response === '1') {
-
-			// Script is running, set the button to 'Stop'
-			document.getElementById(html_element.id).innerText = 'Stop';
-
-		} else {
-
-			// Script stopped, set the button to 'Run'
-			document.getElementById(html_element.id).innerText = 'Run';
-
-		}
+		if (get_response === '0') set_visuals_running(html_element);
+		else set_visuals_stopped(html_element);
 
 	} catch (error) {
 		console.log(error);
 	}
 }
 
-// ! TODO: periodic check to change state automatically & asynchonously
+// Checks the state of all modules/bundles on a page
+async function check_all_state() {
+	const button_element_list = document.getElementsByTagName('button')
+	for (let i = 0; i < button_element_list.length; i++) {
+		check(button_element_list[i]);
+	}
+}
+
+// Periodically check the state of all modules/bundles once the page is loaded
+window.addEventListener('load', function () {
+	(function(){
+		check_all_state();
+		setTimeout(arguments.callee, 10000);
+	})();
+})
--- webserver/php/toggle.php
+++ webserver/php/toggle.php
@@ -1,49 +1,40 @@
 <?php
-
 	$modules_list = array(
 		'mac',
 		'hostname'
 	);
-	$state=false;
 
 	// Toggle a module
-	// Returns the status as a boolean
-	function toggle_module(string $module, bool $state) {
-		$output_state = false;
+	// Returns the shell exit code
+	// 0 = No error
+	// not 0 = Error
+	function exec_module(string $module, string $state = '') {
+		$exit_code = 1;
 
 		// If array exists in list of modules
+		GLOBAL $modules_list;
 		if (in_array($module, $modules_list)) {
-			$exit_code = 1;
-
-			if ($state) {
-				$state_cmd = 'on';
-			} else {
-				$state_cmd = 'off';
-			}
-
-			// Execute system command
-			//exec(string COMMAND, array OUTPUT, int RETURN_VARIABLE);
-			exec("sh ../anon.sh $module $state_cmd", null, $exit_code);
-
-			// Set output status depending on execution
-			if ($exit_code == 0) {
-				$output_state = true;
-			}
+			$cmd="sudo anon $module $state";
+			exec($cmd, $cmd_output, $exit_code);
+			//print_r($cmd_output);
 		}
 
-		return $output_state;
+		return $exit_code;
 	}
 
-	function toggle_test($sstate) {
-		$state = !$state;
-		return $state;
+	if (isset($_GET['value'])) {
+		$value = strip_tags($_GET['value']);
+
+		// If both parameters 'value' and 'state' are set, then pass the toggle command
+		if (isset($_GET['state'])) {
+			$state = strip_tags($_GET['state']);
+			echo exec_module($value, $state);
+		}
+
+		// If only parameter 'value' is set, then pass the check command
+		else echo exec_module($value);
 	}
-
-	// ! TODO: add support for toggle_module()
-//	if (ifset()) {
-		# code...
-//	}
-
-	echo toggle_test($_GET['state']);
-	//echo toggle_module($_GET['value'], $_GET['state']);
+	
+	// If no parameter is set, then return an error
+	else echo '1';
 ?>
