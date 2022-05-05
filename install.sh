9974c9f9df1d957eda031c77aa79cc892cd3044a
r-jb <24355753+r-jb@users.noreply.github.com>
2022-05-05 22:47:56 +0100

    Fixed install script

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+DATA_DIR='/usr/share'
+PROGRAM_DIR='/usr/local/bin'
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 RED='\033[0;31m'
@@ -11,85 +13,74 @@ echo_success() {
 }
 
 echo_info() {
-	echo "${BLUE}[i] - ${@}${NOCOLOR} !"
+	echo "${BLUE}[i] - ${@}${NOCOLOR}"
 }
 
 echo_error() {
 	echo "${RED}[-] - ${@}${NOCOLOR}"
 }
 
-function init {
-	if (($EUID != 0)); then
-		echo "[!] Please run with root permissions"
-		exit 1
-	fi
+cmd_exist() {
+	command -v "$1" >/dev/null 2>/dev/null
+	return $?
 }
 
-function check_requirements {
-	[ ! -d "$DIR/lib" ] && mkdir "$DIR/lib"
-
-	cd "$DIR/lib"
-
-	if [ ! -d /usr/bin/nipe ]; then
-		echo "[!] nipe $msg"
-		exit 1
-	fi
-
-	if [ ! -e /usr/bin/macchanger ]; then
-		echo "[!] macchanger $msg"
-		exit 1
-	fi
-
-	if [ ! -d /usr/bin/web-traffic-generator ]; then
-		echo "[!] web-traffic-generator $msg"
-		exit 1
-	fi
+install() {
+	sudo apt update
+	cmd_exist anon || install_anon
+	cmd_exist kalitorify || install_kalitorify
 }
 
-function install {
-	dir="/tmp/"
-	cd "$dir"
+install_package() {
+	echo_info "Installing $1"
+	apt install -y --no-install-recommends "$1" > /dev/null && \
+	echo_success "Installed $1"
+}
 
-	#macchanger perl python pip git
-	apt update && \
-	apt -y install macchanger perl python python-pip git
-	apt -y autoremove
+install_anon() {
+	cmd_exist git || install_package git
+	cmd_exist php || install_package php
+	cmd_exist macchanger || install_package macchanger
 
-	#nipe
-	git clone https://github.com/GouveaHeitor/nipe && \
-	cp -a --remove-destination nipe /usr/bin/ && \
-	cd /usr/bin/nipe && \
-	perl nipe.pl install && \
-	cpan install Switch JSON Config::Simple
-	cd "$dir"
+	echo_info 'Installing anon'
+	sudo git clone -q https://github.com/r-jb/anon "$DATA_DIR/anon" && \
+	sudo chmod +x "$DATA_DIR/anon/anon.sh" && \
+	sudo ln -sf "$DATA_DIR/anon/anon.sh" "$PROGRAM_DIR/anon"
 
-	#web-traffic-generator
-	if [ -f /usr/bin/web-traffic-generator/config.py ]; then
-		wget https://raw.githubusercontent.com/ReconInfoSec/web-traffic-generator/master/gen.py && \
-		cp -a --remove-destination web-traffic-generator/gen.py /usr/bin/web-traffic-generator/
+	# Add script to sudoers to allow running sudo without prompring for password
+	# Needed for the webui
+	if [ "$EUID" = '0' ]
+	then
+		echo_info 'If you want to run the script as a regular user, consider running the install script as the user in question without sudo'
 	else
-		pip install requests && \
-		git clone https://github.com/ReconInfoSec/web-traffic-generator && \
-		cp -a --remove-destination web-traffic-generator /usr/bin/
+		echo "$USER ALL=(root) NOPASSWD: $PROGRAM_DIR/anon" | sudo tee -a "/etc/sudoers.d/$USER"
 	fi
 
-	cd ~
-	rm -rf "$dir"
+	echo_success 'Installed anon'
+	install_web_traffic_generator
 }
 
 install_kalitorify() {
+	cmd_exist git || install_package git
 	echo_info 'Installing Kalitorify'
-	apt update && \
-	apt install -y --no-install-recommends tor curl git make && \
-	git clone https://github.com/brainfucksec/kalitorify && \
-	cd kalitorify && \
-	make install && \
+	[ -e "/tmp/kalitorify" ] && sudo rm -rf "/tmp/kalitorify"
+	sudo apt install -y --no-install-recommends tor curl git make && \
+	sudo git clone -q https://github.com/brainfucksec/kalitorify "/tmp/kalitorify" && \
+	cd "/tmp/kalitorify" && \
+	sudo make install && \
+	sudo rm -rf "/tmp/kalitorify" && \
 	echo_success 'Installed Kalitorify'
 }
 
-install_php() {
-	echo_info 'Installing PHP'
-	apt update && \
-	apt install -y --no-install-recommends php && \
-	echo_success 'Installed PHP'
+install_web_traffic_generator() {
+	cmd_exist git || install_package git
+	cmd_exist python || install_package python
+	cmd_exist pip || install_package python-pip
+	sudo mkdir -p "$DATA_DIR/anon/lib"
+	echo_info 'Installing web-traffic-generator'
+	pip install requests && \
+	sudo wget -q -O "$DATA_DIR/anon/lib/gen.py" https://raw.githubusercontent.com/ReconInfoSec/web-traffic-generator/master/gen.py && \
+	echo_success 'Installed web-traffic-generator'
 }
+
+install
